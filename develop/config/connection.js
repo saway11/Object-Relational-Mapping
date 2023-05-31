2b1cd5f15cc27ef85a107ba496423f492ba1c80e
saway11 <rgarcia20.m@gmail.com>
2023-05-30 19:13:34 -0700

    sql script that creates the necessary databases

--- develop/config/connection.js
+++ develop/config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// crete connection to our db
+// create connection to our db
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -1,5 +1,36 @@
--- DROP DATABASE
+-- SQL script that creates the necessary database tables for an e-commerce application
 DROP DATABASE IF EXISTS ecommerce_db;
+CREATE DATABSE ecommerce_db;
 
--- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
+
+CREATE TABLE Category (
+    id INT AUTO_INCREMENT NOT NULL,
+    category_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Product (
+    id INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    category_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES Category(id)
+);
+
+CREATE TABLE Tag (
+    id INT AUTO_INCREMENT NOT NULL,
+    tag_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ProductTag (
+    id INT AUTO_INCREMENT NOT NULL,
+    product_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (product_id) REFERENCES Product(id),
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)
+);
